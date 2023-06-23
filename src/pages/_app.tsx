0804287da7bb020aed850030dc8bef217416535b
Marcos Souza <marcos.souza@beeleads.com.br>
2023-06-22 22:33:33 -0300

    feat: visual structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { AppProps } from "next/app"
+import Image from 'next/image'
+import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
 
-export default MyApp
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,55 @@
+import { styled } from "../styles" 
+import { HomeContainer, Product } from "../styles/pages/home"
+import { useKeenSlider } from 'keen-slider/react'
+
+import tshirt1 from '../assets/tshirts/1.png'
+import tshirt2 from '../assets/tshirts/2.png'
+import tshirt3 from '../assets/tshirts/3.png'
+import Image from "next/image"
+import 'keen-slider/keen-slider.min.css'
+
+
+const Button = styled('button', {
+  background: '$rocketseat'
+})
+
 export default function Home() {
+  
+  const [slideRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+  
   return (
-    <h1>Hello Next!</h1>
+    <HomeContainer ref={slideRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={tshirt1} width={520} height={480} alt="tshirt1"/>
+
+        <footer>
+          <strong>T-shirt 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt2} width={520} height={480} alt="tshirt1"/>
+
+        <footer>
+          <strong>T-shirt 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="tshirt1"/>
+
+        <footer>
+          <strong>T-shirt 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
